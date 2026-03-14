626f6c62e1c492b7daa99096629dc7d841d167a9
jfsilval <josef.silvalopez@gmail.com>
2026-03-14 16:35:29 +0100

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,8 @@
-export function middleware() {}
+import createMiddleware from 'next-intl/middleware'
+import { routing } from './i18n/routing'
+
+export default createMiddleware(routing)
 
 export const config = {
-  matcher: [],
+  matcher: ['/((?!api|_next|_vercel|.*\\..*).*)',],
 }
