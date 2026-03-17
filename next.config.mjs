dc306ebcf82c480ab9dce39496c43fbd38e4c235
jfsilval <josef.silvalopez@gmail.com>
2026-03-17 11:23:27 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -26,7 +26,7 @@ const nextConfig = {
             key: 'Content-Security-Policy',
             value: [
               "default-src 'self'",
-              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://va.vercel-scripts.com",
+              "script-src 'self' 'unsafe-inline' https://va.vercel-scripts.com",
               "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
               "font-src 'self' https://fonts.gstatic.com",
               "img-src 'self' data: blob: https://cms-payload-livwell.vercel.app https://*.public.blob.vercel-storage.com",
